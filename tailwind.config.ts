54e327ff75bb54ef129fc2e401bdb95242f4e87e
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2024-09-03 19:49:14 +0530

    Fix overwritten colors in tailwind css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,10 +13,19 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-    },
-    colors: {
-      "brand-heading": "#F8F9FD",
-      "brand-border": "#B7B7B7",
+      colors: {
+        "brand-lightgray": "#F8F9FD",
+        "brand-lightgray-100": "#F4F4F4",
+        "brand-gray": "#B7B7B7",
+        "brand-gray-100": "#D9D9D9",
+        "brand-gray-200": "#666666",
+        "brand-slategray": "#F8F9FD7D",
+        "brand-green": "#44924C",
+        "brand-blue": "#003145",
+        "brand-lightyellow": "#F2C94C40",
+        "brand-yellow": "#FFCE5C",
+        "brand-lightred": "#FF3C002B",
+      },
     },
   },
   plugins: [],
